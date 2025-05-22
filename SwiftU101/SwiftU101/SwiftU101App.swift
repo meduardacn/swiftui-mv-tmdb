b0a3b59af3b25d705b027410a02705fc741f332f
maducasanova <maria.casanova@poatek.com>
2025-05-22 18:08:30 -0300

    Creates project

---
+++ SwiftU101/SwiftU101/SwiftU101App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftU101App.swift
+//  SwiftU101
+//
+//  Created by Maria Casanova on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftU101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
