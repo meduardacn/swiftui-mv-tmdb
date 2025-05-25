26385325d985b0ee7a8a4653e2ab96edf3d993d7
maducasanova <maria.casanova@poatek.com>
2025-05-25 14:57:14 -0300

    Networking: tests

--- SwiftU101/SwiftU101/ContentView.swift
+++ SwiftU101/SwiftU101/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+    let network = NetworkClient()
+
     var body: some View {
         VStack {
             Image(systemName: "swift")
@@ -15,6 +18,14 @@ struct ContentView: View {
                 .frame(width: 50, height: 50)
             Text("SwiftUI")
                 .font(.largeTitle)
+        }
+        .task {
+            do {
+                let image: UIImage = try await network.image(for: .fetchImage(filePath: "wwemzKWzjKYJFfCeiB57q3r4Bcm.png"))
+                let some = Image(uiImage: image)
+            } catch {
+                print(error)
+            }
         }
         .foregroundStyle(.blue)
         .padding()
