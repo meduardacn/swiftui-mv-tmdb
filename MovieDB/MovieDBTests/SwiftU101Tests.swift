66bcd4acef36f6635d1fbb8ad9914312de536db9
maducasanova <maria.casanova@poatek.com>
2025-05-26 14:20:08 -0300

    rename

--- MovieDB/MovieDBTests/SwiftU101Tests.swift
+++ MovieDB/MovieDBTests/SwiftU101Tests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import SwiftU101
 
-final class SwiftU101Tests: XCTestCase {
+final class MovieDBTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
