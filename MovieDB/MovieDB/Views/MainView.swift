6ca7485930735e0f4f0c36f3b7429c0fe76c6ff7
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2025-05-26 23:26:56 -0300

    Add MainView skeleton

--- MovieDB/MovieDB/Views/MainView.swift
+++ MovieDB/MovieDB/Views/MainView.swift
@@ -5,3 +5,26 @@
 //  Created by Maria Eduarda on 26/05/25.
 //
 
+import SwiftUI
+
+struct MainView: View {
+
+    var body: some View {
+        NavigationStack {
+            List {
+            }
+            .listStyle(.plain)
+            .navigationTitle("The MovieDB")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        .environment(
+            NowPlayingMoviesStore(
+//                movieService: MockMoviesService()
+            )
+        )
+        .environment(PopularMoviesStore())
+}
