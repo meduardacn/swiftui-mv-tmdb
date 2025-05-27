0ab1d94fc2b92615c0fa2ea0797984336a248437
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2025-05-26 23:37:03 -0300

    Add Popular Movies store

---
+++ MovieDB/MovieDB/Store/PopularMoviesStore.swift
@@ -0,0 +1,37 @@
+//
+//  PopularMoviesStore.swift
+//  MovieDB
+//
+//  Created by Vinícius Chagas on 26/05/25.
+//
+
+import Observation
+
+@Observable
+final class PopularMoviesStore {
+
+    private(set) var movies: [Movie] = []
+
+    private let movieService: any MoviesService
+
+    init(movieService: any MoviesService = .shared()) {
+        self.movieService = movieService
+    }
+
+    func fetchMovies() async {
+        debugPrint(">>> Fetching popular movies...")
+
+        do {
+            let result = try await movieService.fetchPopularMovies(page: 1)
+            await self.setMovies(result)
+        } catch {
+            debugPrint(">>> Error fetching popular movies: \(error)")
+        }
+    }
+
+    @MainActor
+    private func setMovies(_ movies: [Movie]) {
+        debugPrint(">>> Set popular movies: \(movies.map(\.title))")
+        self.movies = movies
+    }
+}
--- MovieDB/MovieDB/SwiftU101App.swift
+++ MovieDB/MovieDB/SwiftU101App.swift
@@ -14,6 +14,7 @@ struct SwiftU101App: App {
         WindowGroup {
             MainView()
                 .environment(NowPlayingMoviesStore())
+                .environment(PopularMoviesStore())
         }
     }
 }
