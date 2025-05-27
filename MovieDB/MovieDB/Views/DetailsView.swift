d3a798f3ec55c7af3513ac529786b693b660be2b
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2025-05-26 23:38:03 -0300

    Add movie Details view placeholder

--- MovieDB/MovieDB/Views/DetailsView.swift
+++ MovieDB/MovieDB/Views/DetailsView.swift
@@ -5,3 +5,14 @@
 //  Created by Maria Eduarda on 25/05/25.
 //
 
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text("Details View")
+    }
+}
+
+#Preview {
+    DetailsView()
+}
