67aa21815dbacb65492980bb7b6732e2ebb8e4ee
Vinicius Chagas Soares <vinicius.soares@poatek.com>
2025-06-03 14:57:39 -0300

    Optimize fetched image sizes

--- MovieDB/MovieDB/SwiftU101App.swift
+++ MovieDB/MovieDB/SwiftU101App.swift
@@ -10,6 +10,11 @@ import SwiftUI
 @main
 struct SwiftU101App: App {
 
+    init() {
+        URLCache.shared.memoryCapacity = 300_000_000 // ~10 MB memory space
+        URLCache.shared.diskCapacity = 1_000_000_000 // ~1GB disk cache space
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
